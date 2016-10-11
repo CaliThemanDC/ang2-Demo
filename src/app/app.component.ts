fcd728e721ade7851c8b397c0a577affd7de1c3b
Khalil Boyd <Khalil.boyd@gmail.com>
2016-10-11 10:45:29 -0400

    updated .ts and not .js this time, Hi John

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: '<h1>John Dont have too much fun in Vegas</h1><br/><h2>This is my first Angular2 app</h2>'
 })
 export class AppComponent { }
